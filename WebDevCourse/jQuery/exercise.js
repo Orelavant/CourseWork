320ab258e1414d092eee79ed826847d7fc9dff67
Jacob Valero <54868391+Orelavant@users.noreply.github.com>
2020-10-22 11:36:25 -0400

    Finished small jQuery exercise

---
+++ WebDevCourse/jQuery/exercise.js
@@ -0,0 +1,9 @@
+divs = $("div");
+highlight = $(".highlight");
+third = $("#third");
+first = $("div:first-of-type");
+
+divs.css("background-color", "purple");
+highlight.css("width", "200px");
+third.css("border", "1px dashed orange");
+first.css("color", "pink");
